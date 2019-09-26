f8aadcec4b70dcc66de0e801cba87b3f836db662
Cristian Araujo <araujocristian@live.com>
2019-09-26 15:51:05 -0300

    fix api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://omistack-backend.herokuapp.com',
+  baseURL: 'https://omnistack-backend.herokuapp.com',
 });
 
 export default api;
